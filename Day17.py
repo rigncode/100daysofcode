9e1668c14d60e8646d480b3c2e5b9db2518dd5ea
Himanshi ahuja <himansheeahuja@gmail.com>
2020-07-31 00:45:37 +0530

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -1,1 +1,14 @@
 
+
+
+# Forces to scroll horizontal
+def context_in_shot( prop='test_project', assests='t000_a', custom=['IND_01', 'arnold', 'key_LGT_001', 'others']):
+ pass
+
+
+
+# Vertical scroll & compare view possible
+def context_in_shot(prop='test_project', 
+                  assests='t000_a', 
+                  custom=['IND_01', 'arnold', 'key_LGT_001', 'others']):
+ pass
