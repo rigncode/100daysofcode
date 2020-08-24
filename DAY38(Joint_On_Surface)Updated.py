87e94b8cbc5f77535bfd700463284c06d58cd687
Himanshi ahuja <himansheeahuja@gmail.com>
2020-08-24 10:43:14 +0530

    Create DAY38(Joint_On_Surface)Updated.py

---
+++ DAY38(Joint_On_Surface)Updated.py
@@ -0,0 +1,29 @@
+###############################################################################################################
+#################################### Joint_On_Surface##########################################################
+###############################################################################################################
+#Author:Himanshi Ahuja
+#Email:himansheeahuja@gmail.com
+
+#Select (object/vertices/shape) and run the script.
+#It will create joint on that position.
+
+
+
+
+
+
+selected_obj = cmds.ls(sl=True)
+
+create_cluster = cmds.cluster(selected_obj)
+cmds.select(cl=True)   #clear selection
+
+Joint = cmds.joint(n= (str(selected_obj[0])+'_Joint'))
+cmds.delete(cmds.parentConstraint(create_cluster,Joint))
+cmds.delete(create_cluster)
+
+try:
+	newJointName = Joint.split('_')[0]
+
+	cmds.rename(Joint,(newJointName+"_Joint"))
+	
+except:	pass 
